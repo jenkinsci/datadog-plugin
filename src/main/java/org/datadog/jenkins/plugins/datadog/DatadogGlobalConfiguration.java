58b6b6329fa7ae01918c9c93af097c4d200afa2a
Sarah Witt <sarah.witt@datadoghq.com>
2020-04-28 11:20:24 -0400

    Check for zero as port

--- src/main/java/org/datadog/jenkins/plugins/datadog/DatadogGlobalConfiguration.java
+++ src/main/java/org/datadog/jenkins/plugins/datadog/DatadogGlobalConfiguration.java
@@ -32,6 +32,7 @@ import hudson.util.Secret;
 import jenkins.model.GlobalConfiguration;
 import net.sf.json.JSONObject;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.datadog.jenkins.plugins.datadog.clients.ClientFactory;
 import org.datadog.jenkins.plugins.datadog.clients.DatadogHttpClient;
 import org.datadog.jenkins.plugins.datadog.util.SuppressFBWarnings;
@@ -276,7 +277,7 @@ public class DatadogGlobalConfiguration extends GlobalConfiguration {
     }
 
     public static boolean validatePort(String targetPort) {
-        return StringUtils.isNotBlank(targetPort) && StringUtils.isNumeric(targetPort);
+        return StringUtils.isNotBlank(targetPort) && StringUtils.isNumeric(targetPort) && NumberUtils.createInteger(targetPort) != 0;
     }
 
     /**
