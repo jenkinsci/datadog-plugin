28cda2a6e81a7514b67edb1ba57cc12df58841bd
Sarah Witt <sarah.witt@datadoghq.com>
2020-06-08 10:52:28 -0400

    Don't use deprecated method

--- src/main/java/org/datadog/jenkins/plugins/datadog/DatadogUtilities.java
+++ src/main/java/org/datadog/jenkins/plugins/datadog/DatadogUtilities.java
@@ -568,7 +568,7 @@ public class DatadogUtilities {
     }
 
     public static String getJenkinsUrl() {
-        Jenkins jenkins = Jenkins.getInstance();
+        Jenkins jenkins = Jenkins.getInstanceOrNull();
         if(jenkins == null){
             return "unknown";
         }else{
