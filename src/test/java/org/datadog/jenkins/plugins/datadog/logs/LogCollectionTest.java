af802ec5c6141347e23a0e1fe66a3e7d52e33e16
Florian Veaux <florian.veaux@datadoghq.com>
2020-06-18 10:20:57 +0200

    Add support for collection of pipeline logs (#74)
    * Add workflow-job dependency (#72)
    
    * Add workflow-job dependency
    
    * Update version
    
    * Reuse jenkins.version constant
    
    * Bumpt to 2.164.x
    
    * Set min version to 2.164.1
    
    * Make ClientFactory mockable
    
    * Add workflow-job dependency
    
    * Update version
    
    * Reuse jenkins.version constant
    
    * Bumpt to 2.164.x
    
    * Collect pipeline logs
    
    * Set min version to 2.164.1
    
    * Remove Serializable (it already is)
    
    * Add tests
    
    * Fix tests
    
    * Remove debug logging
    
    * Address review
    
    * Update src/test/java/org/datadog/jenkins/plugins/datadog/logs/LogCollectionTest.java
    
    Co-authored-by: Sarah Witt <witt.s@husky.neu.edu>
    
    Co-authored-by: Sarah Witt <witt.s@husky.neu.edu>

---
+++ src/test/java/org/datadog/jenkins/plugins/datadog/logs/LogCollectionTest.java
@@ -0,0 +1,78 @@
+package org.datadog.jenkins.plugins.datadog.logs;
+
+import hudson.ExtensionList;
+import hudson.model.Computer;
+import hudson.model.FreeStyleProject;
+import hudson.model.labels.LabelAtom;
+import hudson.tasks.BatchFile;
+import hudson.tasks.Shell;
+import java.util.logging.Logger;
+import jenkins.model.Jenkins.MasterComputer;
+import org.datadog.jenkins.plugins.datadog.DatadogGlobalConfiguration;
+import org.datadog.jenkins.plugins.datadog.clients.ClientFactory;
+import org.datadog.jenkins.plugins.datadog.clients.DatadogClientStub;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class LogCollectionTest {
+
+    @ClassRule
+    public static JenkinsRule j = new JenkinsRule();
+    private static DatadogClientStub stubClient = new DatadogClientStub();
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        ClientFactory.setTestClient(stubClient);
+        DatadogGlobalConfiguration cfg = ExtensionList.lookup(DatadogGlobalConfiguration.class).get(0);
+        ExtensionList.clearLegacyInstances();
+        cfg.setCollectBuildLogs(true);
+        j.createOnlineSlave(new LabelAtom("test"));
+    }
+
+
+    @Test
+    public void testFreeStyleProject() throws Exception {
+        FreeStyleProject p = j.createFreeStyleProject();
+        MasterComputer master = null;
+        for(Computer c: j.jenkins.getComputers()){
+            if(c instanceof MasterComputer){
+                master = (MasterComputer)c;
+                break;
+            }
+        }
+        if(master == null){
+            Assert.fail("Unable to find the master computer.");
+        }
+
+        if(master.isUnix()){
+            p.getBuildersList().add(new Shell("echo foo"));
+        } else {
+            p.getBuildersList().add(new BatchFile("echo foo"));
+        }
+        p.scheduleBuild2(0).get();
+        Assert.assertTrue(stubClient.logLines.contains("foo"));
+    }
+
+    @Test
+    public void testPipeline() throws Exception {
+        WorkflowJob p = j.jenkins.createProject(WorkflowJob.class, "pipeline");
+        p.setDefinition(new CpsFlowDefinition("echo 'foo'\n", true));
+        p.scheduleBuild2(0).get();
+        Assert.assertTrue(stubClient.logLines.contains("foo"));
+    }
+
+    @Test
+    public void testPipelineOnWorkerNode() throws Exception {
+        WorkflowJob p = j.jenkins.createProject(WorkflowJob.class, "pipelineOnSlave");
+        p.setDefinition(new CpsFlowDefinition("node('test') {echo 'foo'}\n", true));
+        p.scheduleBuild2(0).get();
+        Assert.assertTrue(stubClient.logLines.contains("foo"));
+    }
+
+}
