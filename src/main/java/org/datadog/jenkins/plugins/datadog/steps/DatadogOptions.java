55453e82a26c02806d81cd42bfcb8264d805133c
florian.veaux <florian.veaux@datadoghq.com>
2020-07-17 17:44:44 +0200

    Add tests

--- src/main/java/org/datadog/jenkins/plugins/datadog/steps/DatadogOptions.java
+++ src/main/java/org/datadog/jenkins/plugins/datadog/steps/DatadogOptions.java
@@ -1,52 +1,43 @@
 package org.datadog.jenkins.plugins.datadog.steps;
 
 import hudson.EnvVars;
-import hudson.console.AnnotatedLargeText;
-import hudson.model.InvisibleAction;
+import hudson.Extension;
 import hudson.model.Run;
-import java.io.BufferedReader;
+import hudson.model.TaskListener;
 import java.io.PrintStream;
-import java.io.Reader;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map.Entry;
+import java.util.Set;
 import javax.annotation.Nonnull;
-
-import javax.xml.crypto.Data;
+import jenkins.YesNoMaybe;
+import org.datadog.jenkins.plugins.datadog.DatadogUtilities;
 import org.datadog.jenkins.plugins.datadog.logs.DatadogTaskListenerDecorator;
-import org.jenkinsci.plugins.workflow.flow.FlowExecution;
-import org.jenkinsci.plugins.workflow.graph.FlowNode;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.jenkinsci.plugins.workflow.log.LogStorage;
 import org.jenkinsci.plugins.workflow.log.TaskListenerDecorator;
-import org.jenkinsci.plugins.workflow.steps.AbstractStepExecutionImpl;
 import org.jenkinsci.plugins.workflow.steps.BodyExecutionCallback;
 import org.jenkinsci.plugins.workflow.steps.BodyInvoker;
-import org.jenkinsci.plugins.workflow.steps.StepContext;
-import org.kohsuke.stapler.DataBoundConstructor;
-
-import hudson.Extension;
-import hudson.model.TaskListener;
-import java.util.Collections;
-import java.util.Set;
-import jenkins.YesNoMaybe;
 import org.jenkinsci.plugins.workflow.steps.Step;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
 import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
 import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
 /**
  * Pipeline plug-in step for recording time-stamps.
  */
 public class DatadogOptions extends Step implements Serializable {
+
     private static final long serialVersionUID = 1L;
     private boolean collectLogs = false;
     private List<String> tags = new ArrayList<String>();
 
     /** Constructor. */
     @DataBoundConstructor
-    public DatadogOptions() {}
+    public DatadogOptions() {
+    }
 
     public boolean getCollectLogs() {
         return collectLogs;
@@ -74,38 +65,42 @@ public class DatadogOptions extends Step implements Serializable {
 
     private static class ExecutionImpl extends StepExecution {
 
+        private static final long serialVersionUID = 1L;
         private DatadogPipelineAction action;
+
         ExecutionImpl(StepContext context, DatadogPipelineAction action) {
             super(context);
             this.action = action;
         }
 
-        private static final long serialVersionUID = 1L;
-        private transient TaskListener listener;
-        private transient Run<?, ?> run;
-
         /** {@inheritDoc} */
         @Override
         public boolean start() throws Exception {
             StepContext context = getContext();
-            listener = context.get(TaskListener.class);
-            if(listener == null){
-                // Can't even log an error message to the task if listener is null. Not expected.
-                return true;
-            }
+            TaskListener listener = context.get(TaskListener.class);
+            assert listener != null;
             PrintStream taskLogger = listener.getLogger();
-            taskLogger.println("Starting DatadogStep");  // TODO: Debug line, remove me
-            run = context.get(Run.class);
-            if(run == null){
-                taskLogger.println("Unable to find a Run object for this job, the Datadog step will not run.");
-                return true;
+            Run<?, ?> run = context.get(Run.class);
+            if (!(run instanceof WorkflowRun)) {
+                // Really not expected
+                return false;
             }
-            if(run.getAction(DatadogPipelineAction.class) == null) {
+            WorkflowRun workflowRun = (WorkflowRun) run;
+            if (run.getAction(DatadogPipelineAction.class) == null) {
                 run.addAction(action);
             } else {
-                listener.getLogger().println("You already defined a datadog step");
+                taskLogger.println("You already defined a datadog step");
             }
             BodyInvoker invoker = context.newBodyInvoker().withCallback(BodyExecutionCallback.wrap(context));
+            if (this.action.isCollectLogs()) {
+                if (DatadogUtilities.getDatadogGlobalDescriptor().isCollectBuildLogs()) {
+                    taskLogger.println("[Datadog] Logging is already enabled globally, you do not need to specify 'collectLogs: true'");
+                } else {
+                    invoker.withContext(TaskListenerDecorator.merge(
+                            context.get(TaskListenerDecorator.class), new DatadogTaskListenerDecorator(workflowRun))
+                    );
+                }
+            }
             invoker.start();
             return false;
         }
