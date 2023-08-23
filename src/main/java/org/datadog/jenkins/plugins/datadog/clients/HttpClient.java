34bd4a2ae47161043c3ddd8d3d8d0b8c0be69863
ci.logs-backend <ube+ci.logs-backend@datadoghq.com>
2023-08-23 14:30:55 +0200

    Address review comments

--- src/main/java/org/datadog/jenkins/plugins/datadog/clients/HttpClient.java
+++ src/main/java/org/datadog/jenkins/plugins/datadog/clients/HttpClient.java
@@ -35,6 +35,23 @@ import org.eclipse.jetty.util.thread.QueuedThreadPool;
 public class HttpClient {
 
     private static final org.eclipse.jetty.client.HttpClient CLIENT = buildHttpClient();
+
+    private static final String MAX_THREADS_ENV_VAR = "DD_JENKINS_HTTP_CLIENT_MAX_THREADS";
+    private static final String MIN_THREADS_ENV_VAR = "DD_JENKINS_HTTP_CLIENT_MIN_THREADS";
+    private static final String IDLE_THREAD_TIMEOUT_MILLIS_ENV_VAR = "DD_JENKINS_HTTP_CLIENT_IDLE_THREAD_TIMEOUT";
+    private static final String RESERVED_THREADS_ENV_VAR = "DD_JENKINS_HTTP_CLIENT_RESERVED_THREADS";
+    private static final String MAX_REQUEST_RETRIES_ENV_VAR = "DD_JENKINS_HTTP_CLIENT_REQUEST_RETRIES";
+    private static final String INITIAL_RETRY_DELAY_MILLIS_ENV_VAR = "DD_JENKINS_HTTP_CLIENT_INITIAL_RETRY_DELAY";
+    private static final String RETRY_DELAY_FACTOR_ENV_VAR = "DD_JENKINS_HTTP_CLIENT_RETRY_DELAY_FACTOR";
+    private static final String MAX_RESPONSE_LENGTH_BYTES_ENV_VAR = "DD_JENKINS_HTTP_CLIENT_MAX_RESPONSE_LENGTH";
+    private static final int MAX_THREADS_DEFAULT = 64;
+    private static final int MIN_THREADS_DEFAULT = 1;
+    private static final int IDLE_THREAD_TIMEOUT_MILLIS = 60_000;
+    private static final int RESERVED_THREADS_DEFAULT = -1;
+    private static final int MAX_REQUEST_RETRIES_DEFAULT = 5;
+    private static final int INITIAL_RETRY_DELAY_MILLIS_DEFAULT = 100;
+    private static final double RETRY_DELAY_FACTOR_DEFAULT = 2.0;
+    private static final int MAX_RESPONSE_LENGTH_BYTES_DEFAULT = 64 * 1024 * 1024; // 64 MB
     private static volatile hudson.ProxyConfiguration EFFECTIVE_PROXY_CONFIGURATION;
 
     private static final Logger logger = Logger.getLogger(HttpClient.class.getName());
@@ -54,10 +71,10 @@ public class HttpClient {
         };
 
         QueuedThreadPool threadPool = new QueuedThreadPool(
-                64,
-                1,
-                60_000,
-                -1,
+                getEnv(MAX_THREADS_ENV_VAR, MAX_THREADS_DEFAULT),
+                getEnv(MIN_THREADS_ENV_VAR, MIN_THREADS_DEFAULT),
+                getEnv(IDLE_THREAD_TIMEOUT_MILLIS_ENV_VAR, IDLE_THREAD_TIMEOUT_MILLIS),
+                getEnv(RESERVED_THREADS_ENV_VAR, RESERVED_THREADS_DEFAULT),
                 queue,
                 null,
                 threadFactory
@@ -120,7 +137,10 @@ public class HttpClient {
 
     public HttpClient(long timeoutMillis) {
         this.timeoutMillis = timeoutMillis;
-        this.retryPolicyFactory = new HttpRetryPolicy.Factory(5, 100, 2.0);
+        this.retryPolicyFactory = new HttpRetryPolicy.Factory(
+                getEnv(MAX_REQUEST_RETRIES_ENV_VAR, MAX_REQUEST_RETRIES_DEFAULT),
+                getEnv(INITIAL_RETRY_DELAY_MILLIS_ENV_VAR, INITIAL_RETRY_DELAY_MILLIS_DEFAULT),
+                getEnv(RETRY_DELAY_FACTOR_ENV_VAR, RETRY_DELAY_FACTOR_DEFAULT));
     }
 
     public <T> T get(String url, Map<String, String> headers, Function<String, T> responseParser) throws ExecutionException, InterruptedException, TimeoutException {
@@ -243,7 +263,7 @@ public class HttpClient {
 
     private static void executeAsynchronously(Supplier<Request> requestSupplier, HttpRetryPolicy retryPolicy) {
         Request request = requestSupplier.get();
-        request.send(new ResponseListener(2 * 1024 * 1024, requestSupplier, retryPolicy));
+        request.send(new ResponseListener(getEnv(MAX_RESPONSE_LENGTH_BYTES_ENV_VAR, MAX_RESPONSE_LENGTH_BYTES_DEFAULT), requestSupplier, retryPolicy));
     }
 
     private static final class ResponseListener extends BufferingResponseListener {
@@ -270,6 +290,12 @@ public class HttpClient {
                 }
 
                 Response response = result.getResponse();
+                int responseCode = response != null ? response.getStatus() : -1;
+                if (responseCode > 0 && responseCode < 400) {
+                    // successful response
+                    return;
+                }
+
                 if (retryPolicy.shouldRetry(response)) {
                     Thread.sleep(retryPolicy.backoff());
                     requestSupplier.get().send(this);
@@ -287,5 +313,29 @@ public class HttpClient {
         public ResponseProcessingException(String message) {
             super(message);
         }
+    }
+
+    private static int getEnv(String envVar, int defaultValue) {
+        String value = System.getenv(envVar);
+        if (value != null) {
+            try {
+                return Integer.parseInt(value);
+            } catch (Exception e) {
+                DatadogUtilities.severe(logger, null, "Invalid value " + value + " provided for env var " + envVar + ": integer number expected");
+            }
+        }
+        return defaultValue;
+    }
+
+    private static double getEnv(String envVar, double defaultValue) {
+        String value = System.getenv(envVar);
+        if (value != null) {
+            try {
+                return Double.parseDouble(value);
+            } catch (Exception e) {
+                DatadogUtilities.severe(logger, null, "Invalid value " + value + " provided for env var " + envVar + ": double number expected");
+            }
+        }
+        return defaultValue;
     }
 }
