c1335c09a7a7bf3760fa13f3047f94c88cf98be5
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2025-03-27 12:57:12 +0100

    Fix a copy-paste error

--- docker/controller-node/31-create-github-personal-access-token-credentials.groovy
+++ docker/controller-node/31-create-github-personal-access-token-credentials.groovy
@@ -4,11 +4,11 @@ import com.cloudbees.plugins.credentials.domains.Domain
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl
 import jenkins.model.Jenkins
 
-def privateKey = System.getenv("GITHUB_SSH_KEY")
-if (privateKey != null && !privateKey.isEmpty()) {
+def username = System.getenv("GITHUB_USERNAME")
+def password = System.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
+
+if (username != null && !username.isEmpty() && password != null && !password.isEmpty()) {
     def credentialId = 'github-personal-access-token'
-    def username = System.getenv("GITHUB_USERNAME")
-    def password = System.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
     def description = 'Personal Access Token for GitHub Branch Source plugin'
 
     def credentials = new UsernamePasswordCredentialsImpl(
